a8d1b624bd967f2731cd9137804204a2cf0ee607
cjin3 <christopherqjin@gmail.com>
2025-03-14 14:18:04 -0400

    main function

--- main.py
+++ main.py
@@ -6,9 +6,30 @@ misty = Robot()
 currentTime = time.time()
 timeRun = 3 #time program will run in seconds
 
-LIN_SPEED = 30 
-ANG_SPEED = 30
+LIN_SPEED = 30 #linear speed
+ANG_SPEED = 30 #angular speed
 
-while(time.time()-currentTime < timeRun):
-    if (canMoveForward):
-        misty.move(speed, 0);
+def main():
+    run = input("-1 for testing, 0 for autonomous, 1 for shared control")
+    assert(run.isdigit()) 
+    if int(run) == 0:
+        autonomous()
+    elif int(run) == -1:
+        test()
+    elif int(run) == 1:
+        sharedControl()
+    return 0
+    
+
+
+def autonomous():
+    while(time.time()-currentTime < timeRun):
+        if (canMoveForward()):
+            misty.move(LIN_SPEED, 0)
+        else:
+            turn();
+
+def canMoveForward():
+    return 
+
+main()
