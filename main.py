93800ab9ed8b10f1676bfef957c9972a72a7eb2d
cjin3 <christopherqjin@gmail.com>
2025-03-14 14:04:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from mistyPy.GenerateRobot import RobotGenerator
-RobotGenerator("ROBOT-IP_ADDRESS-GOES-HERE")
+from mistyPy.Robot import Robot
+import time
 
+misty = Robot()
+
+currentTime = time.time()
+timeRun = 3 #time program will run in seconds
+
+LIN_SPEED = 30 
+ANG_SPEED = 30
+
+while(time.time()-currentTime < timeRun):
+    if (canMoveForward):
+        misty.move(speed, 0);
