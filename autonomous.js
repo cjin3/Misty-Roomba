0c49a90dab9513da151e48291033c41594edee94
cjin3 <christopherqjin@gmail.com>
2025-03-18 17:54:24 -0400

    javascript

---
+++ autonomous.js
@@ -0,0 +1,36 @@
+misty.Debug("roomba mode");
+
+let time = 10000;
+let foundWall = false;
+
+misty.AddPropertyTest("FrontTOFWall", "SensorPosition", "==", "Center", "string");
+misty.AddPropertyTest("FrontTOFWall", "DistanceInMeters", "<=", 0.2, "double");
+misty.RegisterEvent("FrontTOFWall", "TimeOfFlight", 250);
+
+misty.AddPropertyTest("FrontTOFNoWall", "SensorPosition", "==", "Center", "string")
+misty.AddPropertyTest("FrontTOFNoWall", "DistanceInMeters", ">", 0.2, "double");
+misty.RegisterEvent("FrontTOFNoWall", "TimeOfFlight", 250);
+
+autonomous();
+
+function autonomous(){
+    while (time > 0){
+        misty.DriveTime(30, 0, 0.2);
+        if (foundWall){
+            turn();
+            foundWall = false;
+        }
+        time -= 1;
+    }
+}
+
+function _FrontTOFWall(data){
+    let frontTOF = data.PropertyTestResults[0].PropertyParent;
+    misty.Debug("Found Wall!");
+    misty.Debug(frontTOF.DistanceInMeters);
+    foundWall = true;
+}
+
+function turn(){
+    
+}
